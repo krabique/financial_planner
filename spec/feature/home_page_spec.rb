2d8ff5722bcbf4c0c3ef9be2cfa53038efb8baef
Oleg Larkin <krabique48@gmail.com>
2018-02-20 22:08:33 +0000

    Fix rubocop errors

--- spec/feature/home_page_spec.rb
+++ spec/feature/home_page_spec.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-describe "the home page", :type => :feature do
+describe 'the home page', type: :feature do
   it "prints 'Hello, world!'" do
     visit root_path
 
