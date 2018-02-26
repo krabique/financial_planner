c5155471f79a714ed83f849d21e3870a60b4798f
Oleg Larkin <krabique48@gmail.com>
2018-02-26 21:03:49 +0300

    Implement oauth for google

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable
+         :confirmable, :omniauthable,
+         omniauth_providers: [:google_oauth2]
 
   validates :name, presence: true
+
+  def self.oauth_user_credentials(auth)
+    {
+      name: auth.info['name'],
+      provider: auth.provider,
+      uid: auth.uid,
+      email: auth.info.email,
+      password: Devise.friendly_token[0,20],
+      confirmed_at: Time.current
+    }
+  end
+
+  def self.from_omniauth(auth)
+    user = where(provider: auth.provider, uid: auth.uid)
+      .first_or_create(oauth_user_credentials(auth))
+    user.update!(oauth_user_credentials(auth))
+    user
+  end
 end
