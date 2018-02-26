9f34b4eaa310f402b5374a4debe719b9a0f4225b
Oleg Larkin <krabique48@gmail.com>
2018-02-26 06:11:19 +0300

    Generate custom controllers and views for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations'
+  }
+
   root to: 'home#index'
 end
