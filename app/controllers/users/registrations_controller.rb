818ec9c8f7510048f03feeaf95335676b6c10078
Oleg Larkin <krabique48@gmail.com>
2018-02-26 23:51:33 +0300

    Restrict users with profiles from Google and GitHub from editing their profiles

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
+  before_action :authorize_account_edit, only: %i[edit update]
 
   # GET /resource/sign_up
   # def new
@@ -59,4 +60,21 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  def oauth_provider_humanized(type)
+    case type
+    when 'google_oauth2' then 'Google'
+    when 'github' then 'GitHub'
+    end
+  end
+
+  def authorize_account_edit
+    provider = current_user.provider
+    if provider
+      redirect_to root_path, flash: { error: "Can't edit your profile, " \
+        'since it is linked to ' \
+        "#{oauth_provider_humanized(provider)}. You will have " \
+        ' to change your info there.' }
+    end
+  end
 end
