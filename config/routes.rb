a3bdb0e112e06238dfa558788f6933fcf109dcc1
Oleg Larkin <krabique48@gmail.com>
2018-02-28 04:30:47 +0300

    Move devise views from 'users' to 'devise' and remove redundant devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions',
-    passwords: 'users/passwords',
-    confirmations: 'users/confirmations'
+    registrations: 'users/registrations'
   }
 
   root to: 'home#index'
