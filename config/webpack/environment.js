80f6a9a3c61cf806060e771eb4eafb8ac6c3cf4b
Oleg Larkin <krabique48@gmail.com>
2018-02-28 23:56:39 +0300

    Add and use font-awesome 4.7.0 icons for social networks links in navbar

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,14 @@
 const { environment } = require('@rails/webpacker');
 
 const webpack = require('webpack');
-environment.plugins.set('Provide', new webpack.ProvidePlugin({
+
+
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader',
+});
+
+
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
   jQuery: 'jquery',
   Popper: ['popper.js', 'default']
