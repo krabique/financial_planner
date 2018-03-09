ff2525c02d4a3ecf34ed23d55b1ac7ca222ba31f
Oleg Gavrilenya <krabique48@gmail.com>
2018-03-10 02:03:46 +0300

    Remove 'kind' field from transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -8,10 +8,6 @@ class Transaction < ApplicationRecord
   validates :amount_cents, presence: true, numericality: {
     greater_than_or_equal_to: 0
   }
-  validates :kind, presence: true, inclusion: {
-    in: %w[income expense],
-    message: "Transaction's kind must be either income or expense"
-  }
   validates :comment, length: {maximum: 80}
   validates :user, presence: true
   validates :date, presence: true
--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -3,17 +3,8 @@
 FactoryBot.define do
   factory :transaction do
     amount      { Faker::Number.decimal(2) }
-    kind        'income'
     comment     { Faker::DrWho.quote }
     user
     date { Time.current }
-
-    trait :income do
-      kind    'income'
-    end
-
-    trait :expense do
-      kind    'expense'
-    end
   end
 end
