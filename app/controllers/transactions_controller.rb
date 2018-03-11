0825d3d4a132dad0fb69e894a3c29f76023a109e
Oleg Gavrilenya <krabique48@gmail.com>
2018-03-11 04:02:18 +0300

    Add basic all transactios page

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,6 +3,10 @@
 class TransactionsController < ApplicationController
   # before_action :set_transaction, only: %i[edit update destroy]
 
+  def index
+    @transactions = current_user.transactions
+  end
+
   # def new
   #   @transaction = current_user.entries.new
   # end
