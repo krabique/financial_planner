c636e5167323ebf6f94f2f198b2f9c018781eaa1
Oleg Gavrilenya <krabique48@gmail.com>
2018-03-11 23:33:45 +0300

    Add some icons

--- app/cells/navbar_cell.rb
+++ app/cells/navbar_cell.rb
@@ -10,19 +10,6 @@ class NavbarCell < ApplicationCell
 
   private
 
-  # This is required for icons to render properly.
-  #
-  # Example:
-  #
-  # i.fa.fa-google[aria-hidden="true"]
-  # #=> <i aria-hidden="true" class="fa fa-google"></i>
-  # # (returns as an escaped html text, instead of the actual icon)
-  #
-  # https://github.com/trailblazer/cells-slim/issues/14
-  def tag_builder
-    super.tap { |builder| builder.class_eval { include Cell::Slim::Rails } }
-  end
-
   def current_user_link(path)
     @path = path
     render
