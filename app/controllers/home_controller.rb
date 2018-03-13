ff83f66d7c3223d63b991ca1c1e8c21ea52f36ea
Oleg Gavrilenya <krabique48@gmail.com>
2018-03-14 00:57:38 +0300

    Revamp home page current user logic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  before_action :require_login, only: :index
+
   def index
-    @transaction = current_user.transactions.new if current_user
+    @transaction = current_user.transactions.new
     @transactions = Transaction.last_ten(current_user)
+  end
+
+  private
+
+  def require_login
+    render :landing if current_user.blank?
   end
 end
