e2ac3fd5deda947065b91adf747eb41dd50e27a5
Oleg Gavrilenya <krabique48@gmail.com>
2018-03-14 02:35:02 +0300

    Add spec for transactions controller

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -3,8 +3,12 @@
 FactoryBot.define do
   factory :transaction do
     amount      { Faker::Number.decimal(2) }
-    comment     { Faker::DrWho.quote }
+    comment     { Faker::DrWho.quote[0..79] }
     user
     date { Time.current }
+
+    factory :invalid_transaction do
+      amount { 'abc' }
+    end
   end
 end
