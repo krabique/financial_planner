56763f7e3c20030be99170d4ce67a085ec222e8b
Oleg Gavrilenya <krabique48@gmail.com>
2018-03-16 16:02:58 +0300

    Remove obsolete code from balance service

--- app/services/balance_service.rb
+++ app/services/balance_service.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# This is a service that handles oauth for users
+# This is a service for user's transactions balance
 class BalanceService
   def initialize(user)
     @user = user
@@ -14,29 +14,4 @@ class BalanceService
   private
 
   attr_accessor :user
-
-  def user_credentials
-    @user_credentials ||= oauth_user_credentials
-  end
-
-  def create_or_update_user
-    user = User.where(provider: auth.provider, uid: auth.uid)
-      .first_or_create(user_credentials)
-    user.update!(user_credentials)
-    user
-  end
-
-  def oauth_user_credentials
-    auth_personal_info = auth.info
-
-    {
-      name: auth_personal_info.name,
-      provider: auth.provider,
-      uid: auth.uid,
-      email: auth_personal_info.email,
-      password: Devise.friendly_token[0, 20],
-      confirmed_at: Time.current,
-      remote_avatar_url: auth_personal_info.image
-    }
-  end
 end
