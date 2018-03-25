ea442ecfc23475aefa5ac4f031e451236c33c675
Oleg Gavrilenya <krabique48@gmail.com>
2018-03-25 21:26:27 +0300

    Update balance logic to include expenses

--- app/services/balance_service.rb
+++ app/services/balance_service.rb
@@ -7,11 +7,23 @@ class BalanceService
   end
 
   def balance
-    Transaction.where(user: user).sum(:amount_cents)
-      .to_money / 100
+    incomes - expenses
   end
 
   private
 
   attr_accessor :user
+
+  def incomes
+    sum_query('income')
+  end
+
+  def expenses
+    sum_query('expense')
+  end
+
+  def sum_query(kind)
+    Transaction.where(user: user, kind: kind).sum(:amount_cents)
+      .to_money / 100
+  end
 end
