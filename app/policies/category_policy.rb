68664fefa12c3eb9f9dfa27d44053747522fc8f4
Oleg Gavrilenya <krabique48@gmail.com>
2018-03-27 21:36:06 +0300

    Refactor pundit policies methods

--- app/policies/category_policy.rb
+++ app/policies/category_policy.rb
@@ -2,25 +2,11 @@
 
 # Authorizations for categories
 class CategoryPolicy < ApplicationPolicy
-  def edit?
-    belongs_to_user?
-  end
-
   def update?
-    belongs_to_user?
+    record_belongs_to_user?
   end
 
   def destroy?
-    belongs_to_user?
-  end
-
-  private
-
-  def belongs_to_user?
-    return true if user.present? && user == category.user
-  end
-
-  def category
-    record
+    record_belongs_to_user?
   end
 end
--- app/policies/transaction_policy.rb
+++ app/policies/transaction_policy.rb
@@ -2,25 +2,11 @@
 
 # Authorizations for transactions
 class TransactionPolicy < ApplicationPolicy
-  def edit?
-    belongs_to_user?
-  end
-
   def update?
-    belongs_to_user?
+    record_belongs_to_user?
   end
 
   def destroy?
-    belongs_to_user?
-  end
-
-  private
-
-  def belongs_to_user?
-    return true if user.present? && user == transaction.user
-  end
-
-  def transaction
-    record
+    record_belongs_to_user?
   end
 end
