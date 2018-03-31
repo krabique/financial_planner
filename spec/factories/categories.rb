eedaf444e1f8e8826d98cf970cf8d0787e168f27
Oleg Gavrilenya <krabique48@gmail.com>
2018-03-31 23:58:23 +0300

    Add proper categories factory

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -3,5 +3,15 @@
 FactoryBot.define do
   factory :category do
     name { Faker::DrWho.quote[0..79] }
+
+    factory :category_not_nested do
+      user { categorizable }
+      association :categorizable, factory: :user
+    end
+
+    factory :category_nested do
+      user { categorizable.user }
+      association :categorizable, factory: :category_not_nested
+    end
   end
 end
